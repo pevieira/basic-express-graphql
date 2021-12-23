3f30c20334ea6aafebe46f1317f5e2b321848ba6
Pedro <psivero@rollins.edu>
2021-12-22 21:14:11 -0500

    graphql initial setup without schema

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 const express = require('express')
+const expressGraphQL = require('express-graphql').graphqlHTTP
+
+
+const PORT = 4000
 
 const app = express()
 
-const PORT = 4000
+app.use('/graphql', expressGraphQL({
+    graphiql: true
+}))
 
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT} 🚀`)
